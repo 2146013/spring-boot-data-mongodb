7f67784b05de3b984412ead8116d7208cebcb60d
Daniela pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-09-02 12:04:20 -0500

    Commit final

---
+++ src/main/java/org/ada/school/repository/UserDocument.java
@@ -0,0 +1,73 @@
+package org.ada.school.repository;
+import org.ada.school.dto.UserDto;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+public class UserDocument
+{
+    @Id
+    String id;
+
+    String name;
+
+    @Indexed( unique = true )
+    String email;
+
+    String lastName;
+
+    //Date createdAt;
+
+    public UserDocument() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public UserDocument(UserDto userDto)
+    {
+        this.setName(userDto.getName());
+        this.setEmail(userDto.getEmail());
+        this.setLastName(userDto.getLastName());
+    }
+
+    public void update(UserDto userDto) {
+
+        this.setName(userDto.getName());
+        this.setEmail(userDto.getEmail());
+        this.setLastName(userDto.getLastName());
+    }
+}
+
